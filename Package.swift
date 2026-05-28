1a285911332a04582464c35664151e1261aaa4d8
Storyteller CI <hello@getstoryteller.com>
2026-05-28 14:39:26 +0000

    Release StorytellerLottie 4.6.0

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "StorytellerLottie",
-      url: "https://storyteller.azureedge.net/sdk-ios/storyteller-lottie/4.6.0/StorytellerLottie.zip?checksum=67c67005bae8490e4904bf641922c1b96ed8c7c75cd6c155ca90556b20aee185",
-      checksum: "67c67005bae8490e4904bf641922c1b96ed8c7c75cd6c155ca90556b20aee185"
+      url: "https://storyteller.azureedge.net/sdk-ios/storyteller-lottie/4.6.0/StorytellerLottie.zip",
+      checksum: "359bb0111de5317f713bae39d900365b6da5a64ad3c00fd051f8cd3774ce69bd"
     ),
   ]
 )
